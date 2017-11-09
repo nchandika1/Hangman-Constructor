51391345a66b5afed84d4827684e2c2d13c66760
nchandika1 <nchandika@gmail.com>
2017-11-08 19:04:18 -0800

    More fixes and added coments

--- hangman.js
+++ hangman.js
@@ -1,18 +1,22 @@
 var inquirer = require("inquirer");
 var Word = require("./word.js");
 
-// var words = ['PROGRAM', 'LANGUAGE', 'STRING', 'JAVSCRIPT', 'PYTHON'];
-var words = ['PROGRAM', 'CODE'];
+// Prearranged words for the hangman program
+var words = ['PROGRAM', 'LANGUAGE', 'STRING', 'JAVASCRIPT', 'PYTHON'];
+
+// Array to store word Objects that are created based on the words above
 var wordArray = [];
 
+
+// Function to initalize wordArray with word Objects
 function initWords() {
 	for (var i=0; i<words.length; i++) {
 		var newWord = new Word(words[i]);
 		wordArray.push(newWord);
 	}
-	console.log(wordArray);
 }
 
+// Function to reset all the words for a fresh start of the game
 function resetGame() {
 	console.log("RESET game!");
 	for (var i=0; i < wordArray.length; i++) {
@@ -20,11 +24,15 @@ function resetGame() {
 	}
 }
 
+// Represents the round we are playing. We start with round 0.
+// If all words are finished, user gets a choice to re play the game or end it.
 var round = 0;
 
+// Main function that has the logic for Hangman Game
 function playGame() {
 	console.log("ROUND: " + round);
-	wordArray[round].displayWord();
+	var display_str = wordArray[round].displayWord();
+	console.log(display_str);
 	inquirer.prompt([
 		{
 			name: "guess",
@@ -40,6 +48,7 @@ function playGame() {
 				round++;
 				if (round >= wordArray.length) {
 					console.log("Game Over!");
+					// Check if the user wants to play again
 					inquirer.prompt([ 
 						{
 							type: "confirm",
@@ -50,6 +59,7 @@ function playGame() {
 					]).then(function(answer) {
 						console.log(answer.play);
 						if (answer.play == false) {
+							// User has ended the game
 							return;
 						}
 						// Start the game again!
@@ -58,36 +68,61 @@ function playGame() {
 						playGame();
 					});
 				} else {
+					// We haven't reached the end of the word list. On to next word.
 					playGame();
 				}
 			} else {
+				// Successful guess on the letter. On to the next letter to guess.
 				playGame();
 			}
 		} else {
 			// Guessed wrong
 			wordArray[round].guessCount--;
 			if (wordArray[round].guessCount > 0) {
+				// More guesses left.  Continue guessing letters.
 				console.log("INCORRECT!  " + wordArray[round].guessCount + " guesses left.");
 				playGame();
 			} else {
+				// No more guesses left.  Decide if the user wants to quit or continue to play
 				console.log("INCORRECT! No guesses left!")
 				console.log("Word is: " + wordArray[round].word);
 				round++;
 				if (round >= wordArray.length) {
 					console.log("Game Over!");
-					return;
+					// No more words to play.  Reset the game
+					// User has a choice to play again or quit
+					inquirer.prompt([ 
+						{
+							type: "confirm",
+							name: "play",
+							message: "Do you want to play again? ",
+							default: false
+						}
+					]).then(function(answer) {
+						if (answer.play == false) {
+							return;
+						}
+						// Start the game again!
+						round = 0;
+						resetGame();
+						playGame();
+					});
+				} else {
+					// More words to guess but let us ask the user if he wants to play or quit
+					inquirer.prompt([
+						{
+							type: "confirm",
+							name: "play",
+							message: "Do you want to play more?",
+							default: true
+						}
+					]).then(function(answer) {
+						if (answer.play == false) {
+							return;
+						}
+						playGame();
+					});
 				}
-				inquirer.prompt([
-					{
-						type: "confirm",
-						name: "play",
-						message: "Do you want to play more?",
-						default: true
-					}
-				]).then(function(answers) {
-					round++;
-					playGame();
-				});
 			}
 		}
 	});
--- word.js
+++ word.js
@@ -1,5 +1,8 @@
+
 var Letter = require("./letter.js");
 
+// Utility function to help make an array of letter Objects for a given word
+// Used by the Word Object and the letter object array is stored as a prperty of the word Object
 function makeLetterArray(word) {
 	var array = [];
 	for (var char of word) {
@@ -9,19 +12,35 @@ function makeLetterArray(word) {
 	return array;
 }
 
+// Word Object
+// Uses Letter Object to build an array of letters
+// Calls on letter object to build the display of the letters from the word
+
+// And a bunch of methods to help check on the word for guesses, etc.
 var Word = function(word) {
+	// Actual word 
 	this.word = word;
-	this.guessCount = word.length;
+
+	// Guesses allowed for this word.  
+	// Total length plus 3 additional guesses.
+	this.guessCount = word.length + 3;
+
+	// Make an array of letter objects based on the word
 	this.letterArray = makeLetterArray(this.word);
 	
+	// Method that displays word for the game
+	// Calls Letter object's method to decide whether
+	// to display "_ " or letter based on user's guess.
 	this.displayWord = function() {
 		var str='';
 		for (var letter of this.letterArray) {
 			str =  str + letter.getLetter() + ' ';
 		}
-		console.log(str.trim());
+		return (str.trim());
 	}
 	
+	// Method to update the letters based on user's guess
+	// This simply updates guessed property of the letter Object
 	this.updateWord = function(guess) {
 		for (var letter of this.letterArray) {	
 			if (letter.letter == guess) {
@@ -30,6 +49,7 @@ var Word = function(word) {
 		}
 	}
 
+	// Method to check if the user's guess is right or not
 	this.checkGuess = function(guess) {
 		for (var letter of this.letterArray) {
 			if (letter.letter == guess) {
@@ -39,6 +59,7 @@ var Word = function(word) {
 		return false;
 	}
 
+	// Method to check if the word guessed completely or not
 	this.wordGuessed = function() {
 		for (var letter of this.letterArray) {
 			if (letter.guessed == false) {
@@ -48,6 +69,7 @@ var Word = function(word) {
 		return true;
 	}
 
+	// Method to reset the word Properties to restar the game
 	this.resetWord = function() {
 		for (var letter of this.letterArray) {
 			letter.guessed = false;
